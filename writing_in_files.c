bf58e4603ab96776a8cfb6f2d9d4c4b9c0e5eea9
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2026-03-08 12:31:41 +0400

    Add files via upload

--- writing_in_files.c
+++ writing_in_files.c
@@ -2,12 +2,22 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 int main()
 {
     FILE* reading = fopen("text.txt", "r");
     FILE* writing = fopen("output", "w");
 
-    
+    char buffer[100];
+
+    while(fgets(buffer, 100, reading))
+    {
+        if (strchr(buffer, 'x')) 
+        {
+            fputs(buffer, writing);
+        }
+    }
+
     return 0;
 }
